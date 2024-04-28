13932835dda7781b925ec9eab2e2a8ab4d73c82b
NardeenH <97866278+NardeenH@users.noreply.github.com>
2024-04-28 15:05:43 -0500

    Update Assignment4Ex2.cpp

--- Assignment4Ex2.cpp
+++ Assignment4Ex2.cpp
@@ -1,4 +1,4 @@
-
+//Use a one-dimensional array to solve the following problem: A company pays its salespeople on a commission basis. The salespeople each receive $200 per week plus 9 percent of their gross sales for that week. For example, a salesperson who grosses $5000 in sales in a week receives $200 plus 9 percent of $5000, or a total of $650. Write a program (using an array of counters), for a company with 20 employees, that determines how many of the salespeople earned salaries in each of the following ranges (assume that each salesperson's salary is truncated to an integer amount): a) $200-299 b) $300-399 c) $400-499 d) $500-599 e) $600-699 f) $700-799 g) $800-899 h) $900-999 i) $1000 and over.
 
 #include <iostream>
 using namespace std;
